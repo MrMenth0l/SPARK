e307f510acf4949614ee9404fc83f786fe2643a9
Yehosua Hercules <yehosua.hercules4c@gmail.com>
2026-03-24 19:22:04 -0600

    Initial Commit

---
+++ SPARKTests/SPARKTests.swift
@@ -0,0 +1,16 @@
+//
+//  SPARKTests.swift
+//  SPARKTests
+//
+//  Created by Yehosua Hércules on 24/03/26.
+//
+
+import Testing
+
+struct SPARKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
